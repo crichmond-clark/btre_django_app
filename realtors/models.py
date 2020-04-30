9e63cb3053e0bb5b092d07a51d6b348d47c53f26
Connor Richmond-Clarke <cri@macbook-cri.home>
2020-04-30 13:45:28 +0100

    migrated db tables & installed Pillow

--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+
+class Realtor(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='photos/%y/%m/%d')
+    email = models.CharField(max_length=200)
+    phone = models.CharField(max_length=200)
+    seller_of_month = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(datetime.now)
+
+    def __str__(self):
+        return self.name
