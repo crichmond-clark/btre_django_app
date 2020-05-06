558413ad968985de1930628158ec5ed767d94433
Connor Richmond-Clarke <cri@MacBook-cri.local>
2020-05-06 16:56:05 +0100

    made page titles dynamic

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -32,6 +32,7 @@ def register(request):
                 )
                 user.save()
                 messages.success(request, "You are registered and can now login")
+                return redirect("login")
         else:
             messages.error(request, "Passwords do not match")
             return redirect("register")
@@ -41,6 +42,19 @@ def register(request):
 
 def login(request):
     if request.method == "POST":
+        # GET FORM VAUES
+        username = request.POST["username"]
+        password = request.POST["password"]
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, "You have successfully logged in")
+            return redirect("dashboard")
+        else:
+            messages.error(request, "Username or password is incorrect")
+            return redirect("login")
         # REGISTER USER
         return redirect("login")
     else:
@@ -48,6 +62,10 @@ def login(request):
 
 
 def logout(request):
+
+    if request.method == "POST":
+        auth.logout(request)
+        messages.success(request, "You have successfully logged out")
     return redirect("index")
 
 
