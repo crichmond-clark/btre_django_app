7833645f42e623293e8f0a882505813d731eb014
crichmond-clark <crichmondclark@protonmail.com>
2020-05-12 13:33:39 +0100

    finally got nginx to serve static files

--- btre/urls.py
+++ btre/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
 
+
+from django.contrib.staticfiles.views import serve as serve_static
+from django.views.decorators.cache import never_cache
 
 urlpatterns = [
     path('', include('pages.urls')),
@@ -10,4 +14,9 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('contacts/', include('contacts.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    url(r'^static/(?P<path>.*)$', never_cache(serve_static)),
+] 
+
+
+
+# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
